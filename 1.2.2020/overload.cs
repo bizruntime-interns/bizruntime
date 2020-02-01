ed20b6c2c0562915485902cca9989c76220acde7
basavaraj99 <44079402+basavaraj99@users.noreply.github.com>
2020-02-01 14:24:58 +0530

    Add files via upload

---
+++ 1.2.2020/overload.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace overloading
+{
+	class program
+	{
+		public static int intadd(int x, int y)
+		{
+			return x + y;
+		}
+		public static double doubleadd(double x, double y)
+		{
+			return x + y;
+		}
+		static void Main(string[] args)
+		{
+			int num1 = intadd(8, 9);
+			double num2 = doubleadd(4.8, 5.7);
+			Console.WriteLine("addition of 8 and 9 in int is:" + num1);
+			Console.WriteLine("addition of 4.8 and 5.7 in double is:" + num2);
+		}
+	}
+}
+
+
+
