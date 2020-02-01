056108a55f35d076efc0b4ae51a8e08b8ce7c774
basavaraj99 <44079402+basavaraj99@users.noreply.github.com>
2020-02-01 10:47:17 +0530

    method

---
+++ New folder/method1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace method1
+{
+
+
+	public class Condition
+	{
+		public static void check()
+
+		{
+			int a = 10;
+			if (a < 100)
+				Console.WriteLine("variable a is smaller than 100");
+		}
+		public static void Main()
+		{
+			check();
+		}
+	}
+}
+
+
